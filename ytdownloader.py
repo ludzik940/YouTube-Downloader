82025fe70851026817f0d405ae7b619b37fc7001
Dominik <46494682+ludzik940@users.noreply.github.com>
2022-05-29 11:42:14 +0200

    Update with GUI
    GUI with tkinter

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,22 +1,49 @@
+import tkinter as tk
 from pytube import YouTube
-import time
+from tkinter import *
 
-while True:
-    link = input('Please input a youtube video link ')
+root= tk.Tk()
+
+root.title('YouTube Downloader')
+canvas1 = tk.Canvas(root, width = 500, height = 280,  relief = 'raised')
+canvas1.pack()
+
+label1 = tk.Label(root, text='Download from Youtube')
+label1.config(font=('helvetica', 14))
+canvas1.create_window(250, 25, window=label1)
+
+label2 = tk.Label(root, text='Please input your link')
+label2.config(font=(10))
+canvas1.create_window(250, 100, window=label2)
+
+entry1 = tk.Entry (root)
+entry1.config(font=('helvetica', 12))
+canvas1.create_window(250, 140, width=400, height =30, window=entry1)
+
+
+def download_audio():
+    link = entry1.get()
     video = YouTube(link)
-    print('Video title: ', video.title)
+    video.streams.get_audio_only().download('Downloads/Audio', video.title + '.mp3')
+    label4 = tk.Label(root, text='Utwór ' + video.title + ' został pobrany!')
+    label4.config(font=('helvetica', 10))
+    canvas1.create_window(250, 250, window=label4)
 
-    type = input('Do you want to download audio or video? Type: 1 - audio / 2- video ')
+def download_video():
+    link = entry1.get()
+    video = YouTube(link)
+    video.streams.get_highest_resolution().download('Downloads/Video')
+    label4 = tk.Label(root, text='Film ' + video.title + ' został pobrany!')
+    label4.config(font=('helvetica', 10))
+    canvas1.create_window(250, 250, window=label4)
 
-    if type == '1':
-        time.sleep(1)
-        print('Please wait till your download is complete!')
-        video.streams.get_audio_only().download('Downloads/Audio', video.title + '.mp3')
-        print(video.title, " audio has been successfully downloaded.")
-    elif type == '2':
-        time.sleep(1)
-        print('Please wait till your download is complete!')
-        video.streams.get_highest_resolution().download('Downloads/Video')
-        print(video.title, " video has been successfully downloaded.")
-    else:
-        print('Try Again')
+
+
+button1 = tk.Button(text='Download Audio', command=download_audio, bg='red', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(170, 190, width=150, window=button1)
+
+button2 = tk.Button(text='Download Video', command=download_video, bg='blue', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(330, 190, width=150, window=button2)
+
+
+root.mainloop()
